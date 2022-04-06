68620674e5ce2009f1b1a979b036689a91da7da2
Adrian Rosario <arosario@invisionnaire.com>
2022-04-06 00:27:21 -0500

    a
    a

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-if(navigator.userAgent.match(/Android/i)){
-    window.scrollTo(0,1);
- }
+function hideAddressBar(){
+    if(document.documentElement.scrollHeight<window.outerHeight/window.devicePixelRatio)
+      document.documentElement.style.height=(window.outerHeight/window.devicePixelRatio)+'px';
+    setTimeout(window.scrollTo(1,1),0);
+  }
+  window.addEventListener("load",function(){hideAddressBar();});
+  window.addEventListener("orientationchange",function(){hideAddressBar();});
 
- 
+
 // when i click the mute button mute the audio
 let muteButton = document.getElementById("mute");
 let audio = document.getElementById("bg-music");
